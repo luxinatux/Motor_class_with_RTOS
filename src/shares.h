debc7c3f5b27d199af2e85e47ef5a3650dda3bf9
gegilmore <94640460+gegilmore@users.noreply.github.com>
2021-12-06 19:35:56 -0800

    minor changes

--- src/shares.h
+++ src/shares.h
@@ -7,20 +7,18 @@
 #include <taskshare.h>
 
 extern Share<float> imu_share_gx ("gx Data");
-//extern Share<float> imu_share_gy ("gy Data");
+extern Share<float> imu_share_gy ("gy Data");
 
 extern Share<float> fft_share_gx ("FFT gx Data");
-//extern Share<float> fft_share_gy ("FFT gy Data");
+extern Share<float> fft_share_gy ("FFT gy Data");
 
 extern Queue<float> imu_queue_gx (256, "gx Data");
-//extern Queue<float> imu_queue_gy (256, "gy Data");
+extern Queue<float> imu_queue_gy (256, "gy Data");
 
 extern Queue<float> omega_queue_x (256, "Omega x");
-//extern Queue<float> omega_queue_y (256, "Omega x");
+extern Queue<float> omega_queue_y (256, "Omega x");
 
-extern Queue<float> imu_queue_raw_x (4, "Raw x data");
-//extern Queue<float> imu_queue_raw_y (4, "Raw y data");
-
-extern Share<float> imu_share_raw_x ("fast x Data");
+extern Share<float> imu_share_raw_x (4, "Raw x data"); 
+extern Share<float> imu_share_raw_y (4, "Raw y data");
 
 #endif
