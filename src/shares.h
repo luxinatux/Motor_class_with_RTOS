8d1b1e2478de14fbe85407acec481294edbc21b2
gegilmore <94640460+gegilmore@users.noreply.github.com>
2021-12-06 19:36:59 -0800

    minor change

--- src/shares.h
+++ src/shares.h
@@ -18,7 +18,7 @@ extern Queue<float> imu_queue_gy (256, "gy Data");
 extern Queue<float> omega_queue_x (256, "Omega x");
 extern Queue<float> omega_queue_y (256, "Omega x");
 
-extern Share<float> imu_share_raw_x (4, "Raw x data"); 
-extern Share<float> imu_share_raw_y (4, "Raw y data");
+extern Share<float> imu_share_raw_x ("Raw x data"); 
+extern Share<float> imu_share_raw_y ("Raw y data");
 
 #endif
