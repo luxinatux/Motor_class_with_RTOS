fc5e96dacec02132f244276c911196f113294b84
gegilmore <94640460+gegilmore@users.noreply.github.com>
2021-12-07 23:23:51 -0800

    doxygen

--- src/task_fft.h
+++ src/task_fft.h
@@ -1,8 +1,5 @@
 /** @file main.cpp
- *    This file contains a simple demonstration program for ME507 which uses
- *    FreeRTOS to do multitasking. One of the tasks makes a square wave which
- *    can be viewed and measured with a signal analyzer or oscilloscope, while
- *    other tasks just print fairly useless things to the serial port. 
+ *    This is a header file for the task_fft function
  * 
  *  @author Lucas Martos-Repath & Garret Gilmore
  *  @date   15 Nov 2021 Original file
@@ -12,12 +9,6 @@
 #include <Arduino.h>
 #include <STM32FreeRTOS.h>
 #include <shares.h>
-
-/** @brief   Print a number, saying that it's a number.
- *  @details This is not a particularly useful function, except that it
- *           helps to show how a function can print things under Arduino.
- *  @param   number The number which is to be printed
- */
 
 #ifndef _TASK_FFT_
 #define _TASK_FFT_
