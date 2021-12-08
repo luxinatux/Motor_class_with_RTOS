b36929999b2a9a435c0c02cdd43d23e25e513ce9
gegilmore <94640460+gegilmore@users.noreply.github.com>
2021-12-07 22:33:25 -0800

    doxygen

--- src/task_imu_data.h
+++ src/task_imu_data.h
@@ -1,8 +1,5 @@
 /** @file main.cpp
- *    This file contains a simple demonstration program for ME507 which uses
- *    FreeRTOS to do multitasking. One of the tasks makes a square wave which
- *    can be viewed and measured with a signal analyzer or oscilloscope, while
- *    other tasks just print fairly useless things to the serial port. 
+ *    This is a head file for the task_imu_data function
  * 
  *  @author Lucas Martos-Repath & Garret Gilmore
  *  @date   15 Nov 2021 Original file
@@ -14,12 +11,6 @@
 
 #ifndef _TASK_IMU_DATA_X
 #define _TASK_IMU_DATA_X
-
-/** @brief   Print a number, saying that it's a number.
- *  @details This is not a particularly useful function, except that it
- *           helps to show how a function can print things under Arduino.
- *  @param   number The number which is to be printed
- */
 
 void task_imu_data(void* imu_data);
 
